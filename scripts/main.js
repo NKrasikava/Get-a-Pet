2df962d15d3b5b88e5291d7c89b865fd67d46605
Natallia Krasikava <kraska17@gmail.com>
2019-05-16 21:30:11 +0300

    burger_menu

--- scripts/main.js
+++ scripts/main.js
@@ -4,12 +4,31 @@
 document.addEventListener('DOMContentLoaded', function () {
     var slides = document.querySelectorAll('#slider span'); //с помощью querySelectorAll получаем все слайды из контейнера.
     var i = 0; //создаем переменную для запуска текущего слайда.
-    var interval = setInterval(nextSlide, 2000);//меняем класс для текущего слайда, чтобы спрятать его, задаем интервал 2сек для следующего слайда
+    var interval = setInterval(nextSlide, 3000);//меняем класс для текущего слайда, чтобы спрятать его, задаем интервал 2сек для следующего слайда
 
     function nextSlide() {
         slides[i].className = '';
         i = (i + 1) % slides.length;
         slides[i].className = 'active';
-    }    
-}, false);
+    }
 
+
+    //BURGER
+
+    $('#menu-burger').click(function () {
+        var overlayMenu = $('#overlay-menu'),
+            menuIconLines = $('#menu-burger .menu-icon'),
+            menuBurger = $('#menu-burger');
+
+        if (overlayMenu.hasClass('open')) {
+            overlayMenu.removeClass('open');
+            menuIconLines.removeClass('open');
+            menuBurger.removeClass('open');
+        }
+        else {
+            overlayMenu.addClass('open');
+            menuIconLines.addClass('open');
+            menuBurger.addClass('open');
+        };
+    })
+}, false);
